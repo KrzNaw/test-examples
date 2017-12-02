9fbc0f1920a2c09c492ee2321a3b1141b7b3b7ea
KrzNaw <krzysztof.nawrocki@poczta.onet.pl>
2017-12-02 12:40:17 +0100

    tests created interface and implementation created

---
+++ src/main/java/Calculator/CalculatorImpl.java
@@ -0,0 +1,24 @@
+package Calculator;
+
+public class CalculatorImpl implements Calculator {
+
+    @Override
+    public int add(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int sub(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int multi(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int divide(int a, int b) {
+        return 0;
+    }
+}
--- src/test/java/pl/sda/test/examples/CalculatorTest.java
+++ src/test/java/pl/sda/test/examples/CalculatorTest.java
@@ -1,11 +1,49 @@
 package pl.sda.test.examples;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
 
     @Test
-    public void test(){
+    public void testAdd() {
+        // given
+        int a = 1;
+        int b = 2;
+
+        // when
+        int c = a + b;
+
+        // then
+        Assert.assertEquals("1+2=4", c, 3);
+    }
+
+    @Test
+    public void testSub(){
+
+        double a = 19.9;
+        double b = 11.9;
+
+        double c = a - b;
+
+        Assert.assertEquals(c, 8, 8);
 
     }
+
+    @Test
+    public void testMulti(){
+        int a = 5;
+        int b = 5;
+
+        int c = 5 * 5;
+
+
+
+    }
+
+    @Test
+    public void testDivide(){
+
+    }
+
 }
